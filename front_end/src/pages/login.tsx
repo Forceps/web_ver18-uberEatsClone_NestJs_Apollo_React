e95150a2374234a4fb4944fe544b9f643d859ce0
Forceps <54647455+Forceps@users.noreply.github.com>
2021-01-04 13:20:12 +0900

    s
    s

--- front_end/src/pages/login.tsx
+++ front_end/src/pages/login.tsx
@@ -1,22 +1,16 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, getValues, errors } = useForm();
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className="text-3xl text-gray-800">Log In</h3>
         <form className="flex flex-col mt-5 px-5">
-          <input
-            placeholder="Email"
-            className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-          />
-          <input
-            placeholder="Password"
-            className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600 py-3 px-5 rounded-lg"
-          />
-          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg hover:opacity-90">
-            Log In
-          </button>
+          <input placeholder="Email" className="input mb-3" />
+          <input placeholder="Password" className="input" />
+          <button className="btn mt-3">Log In</button>
         </form>
       </div>
     </div>
